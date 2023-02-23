9de16ea0b2d09dd9c5fbc206e3e3eba8cfe662b2
FlappinPenguin <97498071+FlappinPenguin@users.noreply.github.com>
2023-02-22 21:44:31 -0500

    Update ContactAdd.js

--- API/ContactAdd.js
+++ API/ContactAdd.js
@@ -1,4 +1,13 @@
-var mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -10,9 +19,24 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "insert into Contact_Database values('000-000-0000', 'Testing', 'Stuff', 'TestingStuff@TESTSTUFF.com')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+});
+
+app.post('/ContactAdd', (req, res) => {
+  const { Phone, First_Name, Last_Name, Email } = req.body;
+
+  // Check if contact already exists
+  const sqlSelect = `SELECT * FROM Contact_Database WHERE Phone = '${Phone}'`;
+  connection.query(sqlSelect, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      return res.status(409).json({ message: 'Contact already exists' });
+    }
+
+    // Add new contact
+    const sqlInsert = `INSERT INTO Contact_Database (Phone, First_Name, Last_Name, Email) VALUES ('${Phone}', '${First_Name}', '${Last_Name}', '${Email}')`;
+    connection.query(sqlInsert, (error, result) => {
+      if (error) throw error;
+      return res.status(201).json({ message: 'Contact created'});
+    });
   });
 });
