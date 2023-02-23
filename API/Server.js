a6aa15e27cfe7e56b0d33b32605c5f8134243b22
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-02-22 21:54:59 -0500

    Create Server.js

---
+++ API/Server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+const handleLogin = require('./Login');
+const handleSignup = require'./Signup');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'mydatabase'
+});
+
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Connected to the database');
+});
+
+// Signup + Login endpoint
+app.post('/signup', handleSignup);
+app.post('/login', HandleLogin);
+
+// Start server
+app.listen(3000, () => 
+{
+  console.log('Server running on port 3000')
+});
