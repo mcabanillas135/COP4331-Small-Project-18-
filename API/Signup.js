3690d464d826a26131d05300bd78ae4552b4e48e
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-02-24 15:05:06 -0500

    Update Signup.js

--- API/Signup.js
+++ API/Signup.js
@@ -12,29 +12,35 @@ connection.connect(err => {
   {
     console.error(err); 
     return res.status(501).json({ message: 'Server cannot connect' });
-  };
+  }
   console.log('Connected to the database');
 });
 
 const handleSignup = (req, res) =>
 {
-  const { user, password } = req.body;
+  const { id, user, password } = req.body;
 
   // Check if user already exists
-  const sqlSelect = `SELECT * FROM Contact_User WHERE User_Name = '${user}'`;
+  const sqlSelect = `SELECT * FROM Contact_User WHERE User_Id = '${id}'`;
   connection.query(sqlSelect, (err, results) => {
     if (err) 
     {
       console.error(err);
-    };
-    if (results.length > 0) {
+      return res.status(500).json({ message: 'Server error' });
+    }
+    if (results.length > 0) 
+    {
       return res.status(409).json({ message: 'User already exists' });
     }
 
     // Add new user
-    const sqlInsert = `INSERT INTO Contact_User (user, password) VALUES ('${user}', '${password}')`;
-    connection.query(sqlInsert, (error, result) => {
-      if (error) throw error;
+    const sqlInsert = `INSERT INTO Contact_User VALUES ('${id}', '${user}', '${password}')`;
+    connection.query(sqlInsert, (err, result) => {
+      if (err)
+      {
+        console.error(err);
+        return res.status(500).json({ message: 'Server error' });
+      };
       const newUser = { id: result.insertId, user };
       return res.status(201).json({ message: 'User created', user: newUser });
     });
