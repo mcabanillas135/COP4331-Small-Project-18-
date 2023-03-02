41d1a246b9bac222ee10f9781057f4fc3a801161
MatthewSterling123 <123518768+MatthewSterling123@users.noreply.github.com>
2023-03-02 16:25:20 -0500

    changed const to let

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 login = "http://24.199.121.145/COP4331-Small-Project-18-/Login.php";
 
-const Username = document.getElementById("username");
-const Password = document.getElementById("password");
+let  Username = document.getElementById("username");
+let Password = document.getElementById("password");
 document.getElementById("password");
 
 const options = {
