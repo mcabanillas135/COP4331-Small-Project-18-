1d715534307212644357c9129c69f623312535e3
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-05 04:50:19 -0500

    Update db_connection.php
    exit()

--- API/db_connection.php
+++ API/db_connection.php
@@ -1,22 +1,26 @@
 <?php
 
-    $host = 'cop4332.xyz';
-    $user = 'contactmanager';
-    $password = 'COP4331';
-    $database = 'COP4331';
+	$host = 'cop4332.xyz';
+	$user = 'contactmanager';
+	$password = 'COP4331';
+	$database = 'COP4331';
 
-    // Create connection
-    $conn = new mysqli($host, $user, $password, $database);
+	// Create connection
+	$conn = new mysqli($host, $user, $password, $database);
 
-    // Check connection
-    if ($conn->connect_error) {
-        $retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $conn->connect_error . '"}';
-	header('Content-type: application/json');
-        echo $retValue;
-    }
+	// Check connection
+	if ($conn->connect_error) 
+	{
+		$retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $conn->connect_error . '"}';
+		header('Content-type: application/json');
+		echo $retValue;
+	
+   		// Adding a more explicit script terminator because of science
+		exit()
+    	}
 
-    // Set the charset to UTF-8
-    // uncomment if we know that our database is using utf8, by default sql uses Latin1 or something
-    // mysqli_set_charset($conn, "utf8mb4");
+	// Set the charset to UTF-8
+	// uncomment if we know that our database is using utf8, by default sql uses Latin1 or something
+	// mysqli_set_charset($conn, "utf8mb4");
 
 ?>
