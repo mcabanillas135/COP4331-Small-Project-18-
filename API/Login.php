e9562e9a755516434f2943c1958c60bcdad23589
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-04 22:11:02 -0500

    Update Login.php
    very helpfully commented

--- API/Login.php
+++ API/Login.php
@@ -1,13 +1,18 @@
 <?php
 
 	$inData = getRequestInfo();
-
+	
+	// Imported the connection since it will be the same code across the entire api
+	// Adds a level of encryption as the database login details arent AS visible
 	require_once('db_connection.php');
 	
+	// If the connectino is a success, enter the sql query. Not very safe would be better to have a handleLogin() function instead
+	// Not a big deal for our purposes
 	if( !$conn->connect_error )
 	{
 		$user_info = getUserInfo($conn, $inData['User_Name'], $inData['Password']);
 
+		// If logging in returns a user, return the user with the success code
 		if ($user_info) 
 		{
 			returnWithInfo($user_info['user_name'], $user_info['password'], $user_info['user_id']);
@@ -20,6 +25,7 @@
 	}
 
 	function getUserInfo($conn, $username, $password) {
+		// Preparing the sql query with a statement, much more secure as opposed to the sql_query() function
 		$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ? AND Password = ?");
 		$stmt->bind_param("ss", $username, $password);
 		$stmt->execute();
@@ -27,6 +33,7 @@
 
 		if ($row = $result->fetch_assoc()) 
 		{
+			// Returning it like this makes it easier to format into json if i remember correctly
 			return 
 			[
 				'user_name' => $row['User_Name'],
@@ -43,17 +50,20 @@
 
 	function getRequestInfo()
 	{
+		// Just the normal php syntax for getting a post request i believe
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
 	function sendResultInfoAsJson( $obj )
 	{
+		// Lets the receiver/sender know the data type
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
 	function returnWithError( $err )
 	{
+		// Print an error
 		http_response_code(400);
 		$retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
@@ -61,6 +71,7 @@
 	
 	function returnWithInfo( $user_name, $password, $id )
 	{
+		// Print a success
 		http_response_code(200);
 		$retValue = '{"User_Id":' . $id . ',"User_Name":"' . $user_name . '","Password":"' . $password . '","error":""}';
 		sendResultInfoAsJson( $retValue );
