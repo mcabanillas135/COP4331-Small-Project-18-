f64aec63a14acc7690bf8a12b3bf82528735e23f
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-05 04:29:47 -0500

    Update Signup.php
    should work just as well as the login now. If the login works that is

--- API/Signup.php
+++ API/Signup.php
@@ -1,44 +1,66 @@
 <?php
 
-    $host = 'cop4332.xyz';
-    $user = 'contactmanager';
-    $password = 'COP4331';
-    $database = 'COP4331';
+    $inData = getRequestInfo();
 
-    // Create connection
-    $conn = new mysqli($host, $user, $password, $database);
+    require_once( 'db_connection.php' );
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+	if( !$conn->connect_error ) 
+    {
+        $result = addUser($conn, $inData['User_Id'], $inData['User_Name'], $inData['Password']);
+        
+        if ($result) 
+        {
+            returnWithInfo($inData['User_name'], $inData['User_Id']);
+        } else
+        {
+            returnWithError("Failed to add user");
+        }
+        
+        $conn->close();
     }
-
-    echo "Connected to the database\n";
-
-    function handleSignup($req, $res) {
-        global $conn;
-        $id = $req['id'];
-        $user = $req['user'];
-        $password = $req['password'];
-
-        // Check if user already exists
+    
+    function addUser($conn, $id, $username, $password)
+    {
         $sqlSelect = "SELECT * FROM Contact_User WHERE User_Id = '$id'";
         $result = $conn->query($sqlSelect);
-        if (!$result) {
-            die("Query failed: " . $conn->error);
+        
+        if(!$result || $result->num_rows > 0)
+        {
+           return false;  
         }
-        if ($result->num_rows > 0) {
-            return $res->json(array('message' => 'User already exists'), 409);
-        }
-
-        // Add new user
-        $sqlInsert = "INSERT INTO Contact_User VALUES ('$id', '$user', '$password')";
-        $result = $conn->query($sqlInsert);
-        if (!$result) {
-            die("Query failed: " . $conn->error);
-        }
-        $newUser = array('id' => $conn->insert_id, 'user' => $user);
-        return $res->json(array('message' => 'User created', 'user' => $newUser), 201);
+        
+        $stmt = $conn->prepare("INSERT INTO Contact_User VALUES (?, ?, ?)");
+		$stmt->bind_param("sss", $id, $username, $password);
+		$result = $stmt->execute();
+        $stmt->close();
+        
+        return $result
     }
+
+    function sendResultInfoAsJson( $obj )
+	{
+		// Lets the receiver/sender know the data type
+		header('Content-type: application/json');
+		echo $obj;
+	}
+                
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);   
+    }
+
+    function returnWithError( $err )
+	{
+		http_response_code(400);
+		$retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+                
+    function returnWithInfo( $newUser, $id )
+	{
+		// Print a success
+		http_response_code(200);
+		$retValue = '{"User_Id":' . $id . ',"User_Name":"' . $user_name . '","error":"none"}';
+		sendResultInfoAsJson( $retValue );
+	}
 
 ?>
