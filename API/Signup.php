fb03961f6b9e8b23e84257205e769234cecfd534
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-05 04:52:56 -0500

    Update Signup.php

--- API/Signup.php
+++ API/Signup.php
@@ -1,28 +1,29 @@
 <?php
 
-    $inData = getRequestInfo();
+    	$inData = getRequestInfo();
 
-    require_once( 'db_connection.php' );
+    	require_once( 'db_connection.php' );
+	
+	$result = addUser($conn, $inData['User_Id'], $inData['User_Name'], $inData['Password']);
 
-	if( !$conn->connect_error ) 
-    {
-        $result = addUser($conn, $inData['User_Id'], $inData['User_Name'], $inData['Password']);
-        
-        if ($result) 
-        {
-            returnWithInfo($inData['User_name'], $inData['User_Id']);
-        } else
-        {
-            returnWithError("Failed to add user");
-        }
-        
-        $conn->close();
-    }
+	if ($result) 
+	{
+	    returnWithInfo($inData['User_Name'], $inData['User_Id']);
+	} else
+	{
+	    returnWithError("Failed to add user");
+	}
+
+	$conn->close();
+   	
     
     function addUser($conn, $id, $username, $password)
     {
-        $sqlSelect = "SELECT * FROM Contact_User WHERE User_Id = '$id'";
-        $result = $conn->query($sqlSelect);
+        $stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Id = ? OR User_Name = ?");
+   	$stmt->bind_param("ss", $id, $username);
+   	$stmt->execute();
+    	$result = $stmt->get_result();
+   	$stmt->close();
         
         if(!$result || $result->num_rows > 0)
         {
@@ -30,8 +31,8 @@
         }
         
         $stmt = $conn->prepare("INSERT INTO Contact_User VALUES (?, ?, ?)");
-		$stmt->bind_param("sss", $id, $username, $password);
-		$result = $stmt->execute();
+	$stmt->bind_param("sss", $id, $username, $password);
+	$result = $stmt->execute();
         $stmt->close();
         
         return $result
@@ -51,16 +52,27 @@
     function returnWithError( $err )
 	{
 		http_response_code(400);
-		$retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+	    	$retValue = 
+			[        
+				'User_Id' => 0,
+				'User_Name' => '',
+				'Password' => '',
+				'error' => $err 
+			];
+		sendResultInfoAsJson( json_encode($retValue) );
 	}
                 
-    function returnWithInfo( $newUser, $id )
+    function returnWithInfo( $username, $id )
 	{
 		// Print a success
 		http_response_code(200);
-		$retValue = '{"User_Id":' . $id . ',"User_Name":"' . $user_name . '","error":"none"}';
-		sendResultInfoAsJson( $retValue );
+	    	$retValue =
+			[
+				'User_Id' => $id,
+				'User_Name' => $username,
+				'error' => 'none'
+			];
+		sendResultInfoAsJson( json_encode($retValue) );
 	}
 
 ?>
