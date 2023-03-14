56582a49cf125b7ca4559b06bc051397be47ee3d
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-14 11:35:27 -0400

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,7 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $inData = getRequestInfo();
 
