335873e370b48cef06cbade3d8db96d7b844459d
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-14 11:48:03 -0400

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,55 +1,81 @@
 <?php
-
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'on');
-
 	$inData = getRequestInfo();
-	
-	$firstName = "";
-	$lastName = "";
 
-	$conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
-	if( $conn->connect_error )
+	$username = $inData['User_Name'];
+	$password = $inData['Password'];
+
+	// Imported the connection since it will be the same code across the entire api
+	// Adds a level of encryption as the database login details arent AS visible
+	require_once('db_connection.php');
+
+	// If the connectin is a success, automatically enter the sql query. Not very safe would be better to have a handleLogin() function instead
+	// Not a big deal for our purposes
+	$user_info = getUserInfo($conn, $username, $password);
+
+	$conn->close();
+
+
+	function getUserInfo($conn, $user, $pass) 
 	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ? AND Password = ?");
-		$stmt->bind_param("ss", $inData["User_Name"], $inData["Password"]);
+		// Preparing the sql query with a statement, much more secure as opposed to the sql_query() function
+		$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ? AND Password = ? LIMIT 1");
+		$stmt->bind_param("ss", $user, $pass);
 		$stmt->execute();
 		$result = $stmt->get_result();
-
-		if( $row = $result->fetch_assoc()  )
+		$stmt->close();
+		
+		// If logging in returns a user, return the user with the success code
+		if ($row = $result->fetch_assoc()) 
 		{
-			returnWithInfo( $row['User_Name'], $row['Password']);
-		}
-		else
+			returnWithInfo($row['User_Name'], $row['Password']);
+			return 
+			[
+				'User_Name' => $row['User_Name'],
+				'Password' => $row['Password'],
+			];
+		} else 
 		{
 			returnWithError("No Records Found");
+			return null;
 		}
-		}
-	
+	}
+
 	function getRequestInfo()
 	{
+		// Just the normal php syntax for getting a post request i believe
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
 	function sendResultInfoAsJson( $obj )
 	{
+		// Lets the receiver/sender know the data type
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		// Print an error
+		http_response_code(400);
+		$retValue = 
+			[        
+				'User_Name' => '',
+				'Password' => '',
+				'error' => $err 
+			];
 		sendResultInfoAsJson( $retValue );
 	}
 	
-	function returnWithInfo( $firstName, $lastName)
+	function returnWithInfo( $user_name, $password )
 	{
-		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
+		// Print a success
+		http_response_code(200);
+		$retValue =
+			[
+				'User_Name' => $user_name,
+				'Password' => $password,
+				'error' => 'none'
+			];
 		sendResultInfoAsJson( $retValue );
 	}
 	
