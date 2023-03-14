8b302ea85ed742a33af2ed07dd80d638d38c0808
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-14 11:43:55 -0400

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,40 +1,34 @@
 <?php
 
-    error_reporting(E_ALL);
-    ini_set('display_errors', 'on');
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'on');
 
 	$inData = getRequestInfo();
 	
-	$id = 0;
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "ContactStorage", "contact_storage_pass", "ContactStorageDB"); 	
+	$conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT id,firstName,lastName,password FROM USERS WHERE username=?");
-		$stmt->bind_param("s", $inData["login"]);
+		$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ? AND Password = ?");
+		$stmt->bind_param("ss", $inData["User_Name"], $inData["Password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
-        $row = $result->fetch_assoc();
-    
-		if( $row && password_verify($inData['password'], $row['password']) )
+		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['id'] );
+			returnWithInfo( $row['User_Name'], $row['Password']);
 		}
 		else
 		{
 			returnWithError("No Records Found");
 		}
-
-		$stmt->close();
-		$conn->close();
-	}
+		}
 	
 	function getRequestInfo()
 	{
@@ -53,9 +47,9 @@
 		sendResultInfoAsJson( $retValue );
 	}
 	
-	function returnWithInfo( $firstName, $lastName, $id )
+	function returnWithInfo( $firstName, $lastName)
 	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
+		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
