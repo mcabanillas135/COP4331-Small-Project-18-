fc8ea32560d5ea34c434b814576f3366db753631
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-14 11:39:41 -0400

    Update Login.php

--- API/Login.php
+++ API/Login.php
@@ -1,63 +1,62 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 
-$inData = getRequestInfo();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
 
-$firstName = "";
-$lastName = "";
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
 
-$conn = new mysqli("cop4332.xyz", "contactmanager", "COP4331", "COP4331"); 	
-if( $conn->connect_error )
-{
-	returnWithError( $conn->connect_error );
-}
-else
-{
-	$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ? AND Password = ?");
-	$stmt->bind_param("ss", $inData["User_Name"], $inData["Password"]);
-	$stmt->execute();
-	$result = $stmt->get_result();
-
-	if( $row = $result->fetch_assoc()  )
+	$conn = new mysqli("localhost", "ContactStorage", "contact_storage_pass", "ContactStorageDB"); 	
+	if( $conn->connect_error )
 	{
-		returnWithInfo( $row['User_Name'], $row['Password']);
+		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		returnWithError("No Records Found");
+		$stmt = $conn->prepare("SELECT id,firstName,lastName,password FROM USERS WHERE username=?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+        $row = $result->fetch_assoc();
+    
+		if( $row && password_verify($inData['password'], $row['password']) )
+		{
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['id'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	$stmt->close();
-	$conn->close();
-}
-
-function getRequestInfo()
-{
-	return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson( $obj )
-{
-	header('Content-type: application/json');
-	echo $obj;
-}
-
-function returnWithError( $err )
-{
-	$retValue = '{"User_Id":0,"User_Name":"","Password":"","error":"' . $err . '"}';
-	sendResultInfoAsJson( $retValue );
-}
-
-function returnWithInfo( $firstName, $lastName )
-{
-	$retValue = '{"User_Name":"' . $firstName . '","Password":"' . $lastName . '","error":""}';
-	sendResultInfoAsJson( $retValue );
-}
-
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
-
-
-
-
-
-
