cecbe7083efef6b50249bcbf0e47880747a0d758
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-15 22:51:11 -0400

    Update Signup.php
    Making it functional :)

--- API/Signup.php
+++ API/Signup.php
@@ -1,10 +1,10 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	error_reporting(E_ALL);
-    	ini_set('display_errors', 'on')
-		
     	$inData = getRequestInfo();
-
+	
+	$id = 0;
+	$username = $inData["User_Name"];
+	$password = $inData["Password"];
+	
     	$conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
 
 	if( $conn->connect_error )
@@ -13,40 +13,32 @@
 	}
 	else
 	{
-		$result = addUser($conn, $inData['User_Name'], $inData['Password']);
-
+		$stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ?");
+   		$stmt->bind_param("s", $username);
+   		$stmt->execute();
+    		$result = $stmt->get_result();
+		$stmt->close();
+		
+		if($result->num_rows > 0)
+        	{
+          		return returnWithError("Failed to add user. User already exists.");  
+        	}
+		
+		$stmt = $conn->prepare("INSERT INTO Contact_User VALUES (?, ?)");
+		$stmt->bind_param("ss", $username, $password);
+		$result = $stmt->execute();
+        	
 		if ($result) 
 		{
-		    returnWithInfo($inData['User_Name']);
+		    returnWithInfo($username, $password);
 		} else
 		{
 		    returnWithError("Failed to add user");
 		}
-
+		
+		$stmt->close();
 		$conn->close();
 	}
-   	
-    
-    function addUser($conn, $username, $password)
-    {
-        $stmt = $conn->prepare("SELECT * FROM Contact_User WHERE User_Name = ?");
-   	$stmt->bind_param("s", $username);
-   	$stmt->execute();
-    	$result = $stmt->get_result();
-   	$stmt->close();
-        
-        if(!$result || $result->num_rows > 0)
-        {
-           return false;  
-        }
-        
-        $stmt = $conn->prepare("INSERT INTO Contact_User VALUES (?, ?)");
-	$stmt->bind_param("ss", $username, $password);
-	$result = $stmt->execute();
-        $stmt->close();
-        
-        return $result
-    }
 
     function sendResultInfoAsJson( $obj )
 	{
@@ -62,24 +54,15 @@
     function returnWithError( $err )
 	{
 		http_response_code(400);
-	    	$retValue = 
-			[        
-				'User_Name' => '',
-				'Password' => '',
-				'error' => $err 
-			];
+	    	$retValue = '{"User_Name":"","Password":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( json_encode($retValue) );
 	}
                 
-    function returnWithInfo( $username )
+    function returnWithInfo( $username, $password )
 	{
 		// Print a success
 		http_response_code(200);
-	    	$retValue =
-			[
-				'User_Name' => $username,
-				'error' => 'none'
-			];
+	    	$retValue = $retValue = '{"User_Name":"' . $username . '","Password":"' . $password . '","error":"","success":"Successfully created user"}';
 		sendResultInfoAsJson( json_encode($retValue) );
 	}
 
