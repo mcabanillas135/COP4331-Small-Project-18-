47dfbd680c1bfae162823fe749d68f6055a972d2
FlappinPenguin <97498071+FlappinPenguin@users.noreply.github.com>
2023-03-18 18:03:31 -0400

    Update ContactSearch.php

--- API/ContactSearch.php
+++ API/ContactSearch.php
@@ -1,1 +1,71 @@
+<?php
+    class Contact
+    {
+      public $phone;
+      public $firstname;
+      public $lastname;
+      public $email = "";
+    }
 
+    // Error Testing
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+    $inData = getRequestInfo();
+    $contact = new contact();
+
+    // Required Fields
+    // $id = $inData["User_Id"];
+    $contact->phone = $inData["Phone"];
+    $contact->firstname = $inData["First_Name"];
+    $contact->lastname = $inData["Last_Name"];
+
+    // Not required Fields
+    $contact->email = $inData["Email"];
+
+    $conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
+
+    if( $conn->connect_error )
+    {
+       returnWithError( 0, $conn->connect_error );
+    }
+    else
+    {
+       $stmt = $conn->prepare("SELECT * FROM Contact_database WHERE Phone = ? AND First_Name = ? AND Last_Name = ? AND Email = ?");
+       $stmt->bind_param("ssss", $contact->phone, $contact->firstname, $contact->lastname, $contact->email);
+       $stmt->execute();
+       $result = $stmt->get_result();
+
+       if( $row = $result->fetch_assoc() )
+       {
+	       returnWithInfo("$contact");
+       }
+       else
+       {
+         returnWithEror("Contact does not exist.");
+       }
+
+    function getRequestInfo()
+    {
+       return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+       header('Content-type: application/json');
+       echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+       $retValue = '{"Contact User":"","Phone":"","First_Name":"","Last_Name":"","Email":"","error":"' . $err . '"}';
+       sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo( $contact ) 
+    {
+        $retValue = '{"Contact User":"' . $contact->username . '","Phone":"' . $contact->phone . '","First_Name":"' . $contact->firstname . '","Last_Name":"' . $contact->lastname . '","Email":"' . $contact->email . '","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+	
+?>
