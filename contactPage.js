4efed783d321cca4a5f261221b186a44454da050
QuinnG <quinngilbert12and3@gmail.com>
2023-03-18 15:50:24 -0400

    added highlight functionaity

--- contactPage.js
+++ contactPage.js
@@ -4,6 +4,76 @@ const phoneNumber = document.getElementById('phoneNumber');
 const email = document.getElementById('email');
 const addContactButton = document.getElementById('addContactButton');
 const contactTable = document.getElementById('contactList');
+const tableRows = contactTable.getElementsByTagName('tr');
+
+const contactDetailsPage = document.getElementById('contactDetails');
+const detailedFirstName = document.getElementById('detailedFirstName');
+const detailedLastName = document.getElementById('detailedLastName');
+const detailedEmail = document.getElementById('detailedEmail');
+const detailedPhone = document.getElementById('detailedPhone');
+const street = document.getElementById('street');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const zip = document.getElementById('zip');
+const birth = document.getElementById('birth');
+const added = document.getElementById('added');
+const edited = document.getElementById('edited');
+
+// adds the highlight function (adds "selected" class to highlighted row)
+for(let i=0; i < tableRows.length; i++) {
+	const row = tableRows[i];
+	row.addEventListener('click', selectRow);
+}
+
+function selectRow() {
+	if(this.classList.contains('selected')){
+		this.classList.remove('selected');
+		contactDetailsPage.style.display = 'none';
+		return;
+	}
+	// remove previously selected row
+	for(let i=0; i < tableRows.length; i++) {
+		tableRows[i].classList.remove('selected');
+	}
+	this.classList.add('selected');
+	
+	fillDetailed(this);
+	
+}
+
+function fillDetailed(selectedRow) {
+	// api call that fills information for detailed tableRows
+	
+	contactDetailsPage.style.display = 'block';
+	
+	const cells = selectedRow.querySelectorAll('td');
+	const fName = cells[0].textContent;
+	const lName = cells[1].textContent;
+	const phone = cells[2].textContent;
+	const email = cells[3].textContent;
+	const STREET = "";
+	const CITY = "";
+	const STATE = "";
+	const ZIP = "";
+	const BIRTH = "";
+	const ADDED = "";
+	const EDITED = "";
+	
+	detailedFirstName.value = fName;
+	detailedLastName.value = lName;
+	detailedPhone.value = phone;
+	detailedEmail.value = email;
+	street.value = STREET;
+	city.value = CITY;
+	state.value = STATE;
+	zip.value = ZIP;
+	birth.value = BIRTH;
+	added.value = ADDED;
+	edited.value = EDITED;
+	
+	
+}
+	
 //adds the contact button
 addContactButton.addEventListener("click", e => {
     e.preventDefault();
