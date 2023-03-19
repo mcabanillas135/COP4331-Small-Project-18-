ff58974d4165978c83cd51fd61b49e7f665f935a
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-18 21:38:10 -0400

    Update ContactAdd.php
    updated for new database

--- API/ContactAdd.php
+++ API/ContactAdd.php
@@ -1,86 +1,105 @@
 <?php
-    class Contact
+  class Contact
+  {
+    public $username;
+    public $firstname;
+    public $lastname;
+    public $phone;
+    public $email;
+    public $street = "";
+    public $city = "";
+    public $state = "";
+    public $zip = "";
+    public $dob = "";
+    public $datecreated;
+  }
+
+  // Error Testing
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'on');
+
+  $inData = getRequestInfo();
+  $contact = new Contact();
+
+  // these values are not allowed to be null
+  $contact->username = $inData["User_Name"];
+  $contact->firstname = $inData["FName"];
+  $contact->lastname = $inData["LName"];
+  $contact->phone = $inData["Phone"];
+  $contact->email = $inData["Email"];
+  $contact->street = $inData["Street"];
+  $contact->city = $inData["City"];
+
+  // can be null
+  $contact->state = $inData["State"];
+
+  // an int
+  $contact->zip = $inData["Zip_Code"];
+
+  // a Date
+  $contact->dob = $inData["DOB"];
+  $contact->datecreated = $inData["Date_Created"];
+
+  $conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331");
+
+  if( $conn->connect_error )
+  {
+    returnWithError( $conn->connect_error );
+  }
+  else
+  {
+    $stmt = $conn->prepare("SELECT * FROM Contact_database WHERE Phone = ?");
+    $stmt->bind_param("s", $contact->phone);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if( $row = $result->fetch_assoc() )
     {
-	public $phone;
-	public $firstname;
-	public $lastname;
-	public $email = "";
-	public $datecreated;
-    }
-
-    // Error Testing
-    error_reporting(E_ALL);
-    ini_set('display_errors', 'on');
-
-    $inData = getRequestInfo();
-    $contact = new Contact();
-
-    // Required Fields
-    $contact->phone = $inData["Phone"];
-    $contact->firstname = $inData["First_Name"];
-    $contact->lastname = $inData["Last_Name"];
-    $contact->email = $inData["Email"];
-    $contact->datecreated = $inData["Date_Created"];
-	
-    $conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
-
-    if( $conn->connect_error )
-    {
-       returnWithError( $conn->connect_error );
+      returnWithError("Failed to add contact. Phone Number already exists.");
     }
     else
     {
-       $stmt = $conn->prepare("SELECT * FROM Contact_database WHERE Phone = ? OR Email = ?");
-       $stmt->bind_param("ss", $contact->phone, $contact->email);
-       $stmt->execute();
-       $result = $stmt->get_result();
+      $stmt2 = $conn->prepare("INSERT INTO Contact_database VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+      $stmt2->bind_param("sssssssssss", $contact->username, $contact->firstname, $contact->lastname, $contact->phone, $contact->email,
+                          $contact->street, $contact->city, $contact->state, $contact->zip, $contact->dob, $contact->datecreated);
+      $result = $stmt2->execute();
 
-       if( $row = $result->fetch_assoc() )
-       {
-	       returnWithError("Failed to add contact. Phone Number or Email already exists.");
-       }
-       else
-       {
-	       $stmt2 = $conn->prepare("INSERT INTO Contact_database VALUES (?, ?, ?, ?, ?)");
-	       $stmt2->bind_param("sssss, $contact->phone, $contact->firstname, $contact->lastname, $contact->email, $contact->datecreated";
-	       $result = $stmt2->execute();
-	       
-	       if ($result) 
-	       {
-		       returnWithInfo($contact);
-	       } else
-	       {
-		       returnWithError("Failed to add contact");
-	       }
-			
-	       $stmt2->close();
-       }
+      if ($result)
+      {
+        returnWithInfo($contact);
+      } else
+      {
+        returnWithError("Failed to add contact");
+      }
 
-       $stmt->close();
-       $conn->close();
+      $stmt2->close();
     }
 
-    function getRequestInfo()
-    {
-       return json_decode(file_get_contents('php://input'), true);
-    }
+    $stmt->close();
+    $conn->close();
+  }
 
-    function sendResultInfoAsJson( $obj )
-    {
-       header('Content-type: application/json');
-       echo $obj;
-    }
+  function getRequestInfo()
+  {
+    return json_decode(file_get_contents('php://input'), true);
+  }
 
-    function returnWithError( $err )
-    {
-       $retValue = '{"Phone":"","First_Name":"","Last_Name":"","Email":"","error":"' . $err . '"}';
-       sendResultInfoAsJson( $retValue );
-    }
+  function sendResultInfoAsJson( $obj )
+  {
+    header('Content-type: application/json');
+    echo $obj;
+  }
 
-    function returnWithInfo( $contact ) 
-    {
-        $retValue = '{"Phone":"' . $contact->phone . '","First_Name":"' . $contact->firstname . '","Last_Name":"' . $contact->lastname . '","Email":"' . $contact->email . '","Date created":"' . $contact->datecreated .'","error":"", "success":"Successfully added contact."}';
-        sendResultInfoAsJson( $retValue );
-    }
-	
+  function returnWithError( $err )
+  {
+    $retValue = '{"User_Name":"","FName":"","LName":"","Phone":"","Email":"","Street":"","City":"","State":"","Zip_Code":"","DOB":"","Date_Created":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+  }
+
+  function returnWithInfo( $contact )
+  {
+    $retValue = '{"User_Name":"' . $contact->username . '","FName":"' . $contact->firstname . '","LName":"' . $contact->lastname . '","Phone":"' . $contact->phone . '","Email":"' . $contact->email . '","Street":"' . $contact->street . '","City":"' . $contact->city . '","State":"' . $contact->state . '","Zip_Code":"' . $contact->zip . '","DOB":"' . $contact->dob . '","Date_Created":"' . $contact->datecreated . '","error":"", "success":"Successfully added contact."}';
+    sendResultInfoAsJson( $retValue );
+  }
+
 ?>
