c7b09ceeea7ae736fc89db469f68faf01e50df27
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-18 23:47:55 -0400

    Add files via upload

--- API/Login.php
+++ API/Login.php
@@ -1,15 +1,15 @@
 <?php
 	// Error Testing
 	// error_reporting(E_ALL);
-    	// ini_set('display_errors', 'on');
+  // ini_set('display_errors', 'on');
 
 	$inData = getRequestInfo();
-	
+
 	$id = 0;
 	$username = $inData["User_Name"];
 	$password = $inData["Password"];
 
-	$conn = new mysqli("localhost", "contactmanager", "COP4331", "COP4331"); 	
+	$conn = new mysqli( "localhost", "contactmanager", "COP4331", "COP4331" );
 
 	if( $conn->connect_error )
 	{
@@ -17,24 +17,24 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT * FROM Contact_user WHERE User_Name = ? AND Password = ?");
-		$stmt->bind_param("ss", $username, $password);
+		$stmt = $conn->prepare( "SELECT * FROM Contact_user WHERE User_Name = ? AND Password = ?" );
+		$stmt->bind_param( "ss", $username, $password );
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc() )
 		{
-			returnWithInfo( $row['User_Name'], $row['Password']);
+			returnWithInfo( $row['User_Id'], $row['User_Name'], $row['Password'] );
 		}
 		else
 		{
-			returnWithError("No Records Found");
+			returnWithError( "No Records Found" );
 		}
-		
+
 		$stmt->close();
 		$conn->close();
 	}
-	
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -45,17 +45,17 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
-		$retValue = '{"User_Name":"","Password":"","error":"' . $err . '"}';
+		$retValue = '{"User_Id":"","User_Name":"","Password":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-	function returnWithInfo( $user, $pass)
+
+	function returnWithInfo( $id, $user, $pass )
 	{
-		$retValue = '{"User_Name":"' . $user . '","Password":"' . $pass . '","error":"","success":"Successfully logged in"}';
+		$retValue = '{"User_Id":"' . $id . '","User_Name":"' . $user . '","Password":"' . $pass . '","error":"","success":"Successfully logged in"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
