5c1493adccf61c83be84863b9aeea07d3864e691
FlappinPenguin <97498071+FlappinPenguin@users.noreply.github.com>
2023-03-19 18:47:40 -0400

    Update ContactUpdate.php

--- API/ContactUpdate.php
+++ API/ContactUpdate.php
@@ -7,7 +7,6 @@
   {
     public $firstname;
     public $lastname;
-    public $phone;
     public $email;
     public $street = "";
     public $city = "";
@@ -21,7 +20,7 @@
   $inData = getRequestInfo();
   $contact = new Contact();
 
-  $id = $inData["User_Id"];
+  $phone = $inData["Phone"];
 
   // these values are not allowed to be null
   $contact->firstname = $inData["FName"];
@@ -47,14 +46,14 @@
 	else
 	{
 
-		$stmt1 = $conn->prepare("UPDATE Contacts SET FName = ?, LName = ?, Phone = ?, Email = ?, Street = ?, City = ?, State = ?, Zip_Code = ?, DOB = ? WHERE User_Id = ?");
-		$stmt1->bind_param("sssssssssi", $contact->firstname, $contact->lastname, $contact->phone, $contact->email, $contact->street, $contact->city, $contact->state, $contact->zip, $contact->dob, $id);
+		$stmt1 = $conn->prepare("UPDATE Contacts SET FName = ?, LName = ?, Email = ?, Street = ?, City = ?, State = ?, Zip_Code = ?, DOB = ? WHERE Phone = ?");
+		$stmt1->bind_param("sssssssssi", $contact->firstname, $contact->lastname, $contact->email, $contact->street, $contact->city, $contact->state, $contact->zip, $contact->dob, $phone);
 		$stmt1->execute();
 		$result = $stmt1->get_result();
     
 			if ($result)
 			{
-				returnWithInfo($contact, $id);
+				returnWithInfo($contact, $phone);
 			} else
 			{
 				returnWithError("Failed to update contact.");
@@ -78,13 +77,13 @@
 
 	function returnWithError( $err )
 	{
-		$retValue = '{"User_Id":"","FName":"","LName":"","Phone":"","Email":"","Street":"","City":"","State":"","Zip_Code":"","DOB":"","Date_Created":"","error":"' . $err . '"}';
+		$retValue = '{"FName":"","LName":"","Phone":"","Email":"","Street":"","City":"","State":"","Zip_Code":"","DOB":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
 	function returnWithInfo($contact, $id)
 	{
-		$retValue = '{"User_Id":"' . $id . '","FName":"' . $contact->firstname . '","LName":"' . $contact->lastname . '","Phone":"' . $contact->phone . '","Email":"' . $contact->email . '","Street":"' . $contact->street . '","City":"' . $contact->city . '","State":"' . $contact->state . '","Zip_Code":"' . $contact->zip . '","DOB":"' . $contact->dob . '","Date_Created":"' . $contact->datecreated . '","error":"", "success":"Successfully updated contact."}';
+		$retValue = '{"FName":"' . $contact->firstname . '","LName":"' . $contact->lastname . '","Phone":"' . $contact->phone . '","Email":"' . $contact->email . '","Street":"' . $contact->street . '","City":"' . $contact->city . '","State":"' . $contact->state . '","Zip_Code":"' . $contact->zip . '","DOB":"' . $contact->dob . '","error":"", "success":"Successfully updated contact."}';
     		sendResultInfoAsJson( $retValue );
   	}
 
