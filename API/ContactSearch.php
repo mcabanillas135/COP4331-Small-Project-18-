f4127a10df290777f7d334c3e33c3626cbfadb9e
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-19 18:19:39 -0400

    Update ContactSearch.php

--- API/ContactSearch.php
+++ API/ContactSearch.php
@@ -16,8 +16,8 @@
   }
 
   // Error Testing
-  error_reporting(E_ALL);
-  ini_set('display_errors', 'on');
+  // error_reporting(E_ALL);
+  // ini_set('display_errors', 'on');
 
   $inData = getRequestInfo();
   $contact = new Contact();
