54ab48f5740b1b1c17bfe47f196d7094689cfddf
Glenroy Little <84647047+GSabiniPanini@users.noreply.github.com>
2023-03-22 01:33:51 -0400

    Update and rename EditUser.php to EditUsername.php

--- API/EditUsername.php
+++ API/EditUsername.php
@@ -7,7 +7,6 @@
 
 	$id = $inData["User_Id"];
 	$username = $inData["User_Name"];
-	$password = $inData["Password"];
 
 	$conn = new mysqli( "localhost", "contactmanager", "COP4331", "COP4331" );
 
@@ -23,8 +22,8 @@
 		$result = $stmt->get_result();
 		if ( $row = $result->fetch_assoc() )
 		{
-			$stmt2 = $conn->prepare( "SELECT * FROM Contact_user WHERE User_Name = ? AND NOT User_Id = ?" );
-			$stmt2->bind_param( "si", $username, $id );
+			$stmt2 = $conn->prepare( "SELECT * FROM Contact_user WHERE User_Name = ?" );
+			$stmt2->bind_param( "s", $username );
 			$stmt2->execute();
 			$result = $stmt2->get_result();
 
@@ -34,19 +33,19 @@
 			}
 			else
 			{
-				$stmt3 = $conn->prepare( "UPDATE Contact_user SET User_Name = ?, Password = ? WHERE User_Id = ?" );
-				$stmt3->bind_param( "ssi", $username, $password, $id );
+				$stmt3 = $conn->prepare( "UPDATE Contact_user SET User_Name = ? WHERE User_Id = ?" );
+				$stmt3->bind_param( "si", $username, $id );
 				$stmt3->execute();
 				$affectedRows = $stmt3->affected_rows;
 
 				if ($affectedRows > 0)
 				{
-					returnWithInfo($id, $username, $password);
+					returnWithInfo( $id, $username );
 				}
 				else
 				{
 
-					returnWithError( "Failed to edit user" );
+					returnWithError( "Failed to edit username" );
 
 				}	
 
@@ -78,13 +77,13 @@
 
 	function returnWithError( $err )
 	{
-		$retValue = '{"User_Id":"","User_Name":"","Password":"","error":"' . $err . '"}';
+		$retValue = '{"User_Id":"","User_Name":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
-	function returnWithInfo( $id, $user, $pass )
+	function returnWithInfo( $id, $user )
 	{
-		$retValue = '{"User_Id":"' . $id . '","User_Name":"' . $user . '","Password":"' . $pass . '","error":"","success":"Successfully edited user"}';
+		$retValue = '{"User_Id":"' . $id . '","User_Name":"' . $user . '","error":"","success":"Successfully edited username."}';
 		sendResultInfoAsJson( $retValue );
 	}
 
