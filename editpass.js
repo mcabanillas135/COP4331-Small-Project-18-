b0700e7024151a8ada7e104b478b0a3313aacef2
Matthew Sterling <123518768+MatthewSterlin@users.noreply.github.com>
2023-03-23 17:05:24 -0400

    added code for cancel button

--- editpass.js
+++ editpass.js
@@ -98,6 +98,11 @@ document.addEventListener("DOMContentLoaded", () => { //displays the login or th
         
 
     });
-});
+    document.getElementById("Cancel").onclick = async e =>  {
+        e.preventDefault();
+        window.location.href = "contactPage.html";
 
+        
+    };
+});
 
